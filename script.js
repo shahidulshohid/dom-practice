c00aa807b96d882846bd537f8f75a37ddc80ee69
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-16 02:33:47 +0600

    dom some practice is done

--- script.js
+++ script.js
@@ -4,12 +4,24 @@
 // let parent= grandParent.querySelector(".parent")
 // let child = parent.querySelectorAll(".children")
 // console.log(child);
+
 // let children = document.querySelector(".children")
 // let parent = children.parentElement
 // console.log(parent);
-let children = document.querySelector(".children")
-let grandParent = children.closest(".grandParent")
-console.log(grandParent);
+
+// let children = document.querySelector(".children")
+// let grandParent = children.closest(".grandParent")
+// console.log(grandParent);
+
+// let childrenOne = document.querySelector(".children")
+// let childrenTwo = childrenOne.nextElementSibling
+// console.log(childrenTwo.style.color = "red");
+let childrenTwo = document.querySelector(".children").nextElementSibling;
+let previousChildren = childrenTwo.previousElementSibling
+console.log(previousChildren.style.color = "red");
+
+
+
 
 
 
