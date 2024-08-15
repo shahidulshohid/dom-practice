004952fef2439401486feae9919d6335d836fd99
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-16 00:08:29 +0600

    dom some practice is done

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
-let islam = document.getElementsByClassName("islam")
-let good = islam.innerText
-console.log(good);
+
+// let grandParent = document.querySelector(".grandParent")
+// let parent= grandParent.querySelector(".parent")
+// let child = parent.querySelectorAll(".children")
+// console.log(child);
+let grandParent = document.querySelector(".grandParent")
+let child = grandParent.querySelectorAll(".children")
+console.log(child);
+
 
 
 
