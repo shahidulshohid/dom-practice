0a51151dca5a02c51ccc67dbdbbf9603e97deb56
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-16 00:46:35 +0600

    some practice about closest

--- script.js
+++ script.js
@@ -4,9 +4,15 @@
 // let parent= grandParent.querySelector(".parent")
 // let child = parent.querySelectorAll(".children")
 // console.log(child);
-let grandParent = document.querySelector(".grandParent")
-let child = grandParent.querySelectorAll(".children")
-console.log(child);
+// let children = document.querySelector(".children")
+// let parent = children.parentElement
+// console.log(parent);
+let children = document.querySelector(".children")
+let grandParent = children.closest(".grandParent")
+console.log(grandParent);
+
+
+
 
 
 
