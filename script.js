faa5342a4f9c9c0d9c3023cab7030a7f4d85d4ee
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-16 10:51:15 +0600

    i learnt set the element in html

--- script.js
+++ script.js
@@ -16,9 +16,20 @@
 // let childrenOne = document.querySelector(".children")
 // let childrenTwo = childrenOne.nextElementSibling
 // console.log(childrenTwo.style.color = "red");
-let childrenTwo = document.querySelector(".children").nextElementSibling;
-let previousChildren = childrenTwo.previousElementSibling
-console.log(previousChildren.style.color = "red");
+// let childrenTwo = document.querySelector(".children").nextElementSibling;
+// let previousChildren = childrenTwo.previousElementSibling
+// console.log(previousChildren.style.color = "red");
+
+
+// createElement
+const divElement = document.createElement("div")
+console.log(divElement);
+divElement.className = "red"
+divElement.setAttribute("id", "red div")
+let container = document.querySelector(".grandParent")
+let h1Element = document.querySelector("h1")
+container.insertBefore(divElement, h1Element)
+
 
 
 
